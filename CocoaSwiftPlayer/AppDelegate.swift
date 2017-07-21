3acf0efd0daab6d265f227e689b329f46089191d
priti suthar <priti@jeavio.com>
2017-07-21 18:56:20 +0530

    Initial Commit

---
+++ CocoaSwiftPlayer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CocoaSwiftPlayer
+//
+//  Created by priti suthar on 7/21/17.
+//  Copyright © 2017 priti suthar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CocoaSwiftPlayer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CocoaSwiftPlayer
+//
+//  Created by priti suthar on 7/21/17.
+//  Copyright © 2017 priti suthar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
