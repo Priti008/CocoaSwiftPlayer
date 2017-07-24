8ddcb0cc019f02843b5042029d18e0054ba3ba54
priti suthar <priti@jeavio.com>
2017-07-24 14:15:31 +0530

    Add Podfile

---
+++ DerivedData/CocoaSwiftPlayer/Build/Intermediates/CocoaSwiftPlayer.build/Debug/CocoaSwiftPlayer.build/Script-FF3D1FA7228CB4C2CF634E5B.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+"${SRCROOT}/Pods/Target Support Files/Pods-CocoaSwiftPlayer/Pods-CocoaSwiftPlayer-frameworks.sh"
+
---
+++ DerivedData/CocoaSwiftPlayer/Build/Intermediates/Pods.build/Debug/Pods-CocoaSwiftPlayer.build/DerivedSources/Pods_CocoaSwiftPlayer_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_CocoaSwiftPlayerVersionString[];
+ extern const double Pods_CocoaSwiftPlayerVersionNumber;
+
+ const unsigned char Pods_CocoaSwiftPlayerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_CocoaSwiftPlayer  PROJECT:Pods-1" "\n";
+ const double Pods_CocoaSwiftPlayerVersionNumber __attribute__ ((used)) = (double)1.;
